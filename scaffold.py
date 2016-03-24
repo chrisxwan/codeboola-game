f19beb7697fdcdade76a04ede1b61d1707f7bf34
Christopher Wan <christopher.wan@yale.edu>
2016-03-23 20:32:32 -0400

    scaffold

--- scaffold.py
+++ scaffold.py
@@ -3,25 +3,21 @@ import random
 class Player(object):
 	max_hp = 100
 	current_hp = 100
-	
-	def is_alive(self):
-		return self.current_hp > 0
-	
-	def successful_attack(self):
-		rand = random.random()
-		return rand < self.accuracy
-	
-	def rest(self):
-		if(self.current_hp < self.max_hp):
-			self.current_hp += self.heal
-			if(self.current_hp > self.max_hp):
-				self.current_hp = self.max_hp
-			print("You healed yourself!")
-		else:
-			print("You are already at full health!")
 
+	# Check if the player is still alive 
+	# by ensuring that his current hp is greater than 0
+	def is_alive(self):
+
+	# Return True or False depending on whether the Plaeyer's
+	# attack was successful
+	def successful_attack(self):
+
+	# Heal the Player, if his health is not full already
+	def rest(self):
+
+	# The player has been damaged! Decrease the Player's
+	# current health by the appropriate amount
 	def damage(self, dmg):
-		self.current_hp -= dmg
 
 	def print_current_health(self):
 		print("You currently have " + str(self.current_hp) + " health.")
@@ -37,45 +33,32 @@ class Player(object):
 
 
 class Archer(Player):
+	# input the appropriate attributes for the Archer
 	def __init__(self, name):
-		self.player = "Archer"
-		self.name=name
-		self.accuracy=0.3
-		self.heal=5
-		self.attack=10
 
 class Swordsman(Player):
+	# input the appropriate attributes for the Swordsman
 	def __init__(self, name):
-		self.player = "Swordsman"
-		self.name=name
-		self.accuracy=0.7
-		self.heal=5
-		self.attack=5
 
 class Healer(Player):
+	# input the appropriate attributes for the Healer
 	def __init__(self, name):
-		self.player = "Healer"
-		self.name=name
-		self.accuracy=0.9
-		self.heal=10
-		self.attack=2
-		super(Healer, self).__init__(name, accuracy=0.9, heal = 10, attack = 2)
 
 class GiantSpider():
+	# input the appropriate attributes for the Spider
 	def __init__(self):
-		self.hp = 10
-		self.attack = 2
-		self.accuracy = 0.6
 
+	# Check if the spider is still alive 
+	# by ensuring that its current hp is greater than 0
 	def is_alive(self):
-		return self.hp > 0
 
+	# The player has been damaged! Decrease the Spider's
+	# current health by the appropriate amount
 	def damage(self, dmg):
-		self.hp -= dmg
 
+	# Return True or False depending on whether the Plaeyer's
+	# attack was successful
 	def successful_attack(self):
-		rand = random.random()
-		return rand < self.accuracy
 
 	def print_current_health(self):
 		print("The enemy currently has " + str(self.hp) + " health.\n")
@@ -92,38 +75,41 @@ def fight(player, enemy):
 			print("")
 	
 		if(move == "a"):
-			if(player.successful_attack()):
-				print("You dealt " + str(player.attack) + " damage to the enemy!")
-				enemy.damage(player.attack)
-			else:
-				print("You missed!")
+			# Fill in this code!
+			# What happens if the user wanted to attck?
+		
 		elif(move == "h"):
-			player.rest()
+			# Fill in this code!
+			# What happens if the user wanted to heal?
 			
 		if(enemy.successful_attack()):
-			print("The enemy dealt " + str(enemy.attack) + " to you!\n")
-			player.damage(enemy.attack)
+			# Fill in this code!
+			# What happens if the spider made a successful attack?
+			
 		else:
-			print("The enemy missed its attack.\n")
+			# Fill in this code!
+			# What happens if the spider missed its attack?
 
 	return player.is_alive
 
 name= input("Hello! What's your name? ")
 while(name == ""):
 	name = input("We will need your name to proceed! What is your name? ")
+
 player_class = input("Hi " + name + "! What class of character would you want to be? ")
 player = None
 while(player_class != "a" and player_class != "s" and player_class != "h"):
 	print("")
 	print("Press a to be an archer, press s to be a swordsman, press h to be a healer")
 	player_class = input("What class of character would you want to be? ")
-	
+
+# Based on what the user has chosen, how should you 
+# initialize his/her character?
 if(player_class == "a"):
-	player = Archer(name)
+
 elif(player_class == "s"):
-	player = Swordsman(name)
+
 elif(player_class == "h"):
-	player = Healer(name)
 
 print("")
 player.info()
@@ -134,19 +120,11 @@ while(player.is_alive and num_steps < 10):
 	step = input("What do you want to do? ")
 	print("")
 	if(step == "s"):
-		chance = random.random()
-		if(chance < 0.5):
-			print("You have encountered a spider!")
-			enemy = GiantSpider()
-			battle = fight(player, enemy)
-			if(battle == False):
-				print("You died ): ")
-				break
-			else:
-				print("You have conquered the enemy!\n")
-			
-	
-	
+		# Fill in this code! What happens if a player takes a step?
+		# Hint: In this step, the user either encounters a spider OR
+		# the player does not encounter anything
+		# What is the chance that the player encounters a spider?
+
 		num_steps += 1
 		print("You have made a total of " + str(num_steps) + " steps!\n")
 	elif(step == "q"):
